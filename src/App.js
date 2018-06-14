ab64d8ec38caa301d95119bef817e8a48175035a
Case Western Reserve University <andrewrussmancuso@gmail.com>
2018-06-13 20:31:35 -0400

    added key

--- src/App.js
+++ src/App.js
@@ -65,6 +65,7 @@ class App extends Component {
         {
           this.state.cloths.map(cloth => (
             <ClothCard
+              key={cloths.cloth}
               cloth={cloths.cloth}
               image={cloths.image}
               selectCloth={this.selectCloth}
